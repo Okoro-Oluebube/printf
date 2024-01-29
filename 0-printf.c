0da61e2cb42584232330f18dc7179ac47a55a3d7
Okoro-Oluebube <okorooluebube0000@yahoo.com>
2024-01-29 04:55:07 -0800

    no corrections

---
+++ 0-printf.c
@@ -0,0 +1,67 @@
+#include "main.h"
+/**
+ * _strlen - Number of char in a string
+ * @s: String
+ * Return: Length of string
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+	return (len);
+}
+
+/**
+ * _printf - Produces output according to a format
+ * @format: Format specifier
+ * Return: Integer
+ */
+int _printf(const char *format, ...)
+{
+	va_list fList;
+	int len = 0, i;
+
+	va_start(fList, format);
+	while (*format != '\0')
+	{
+		if (*format != '%')
+		{
+			write(1, format, 1);
+			format++;
+			len++;
+		}
+		else if (*format == '%')
+		{
+			format++;
+			if (*format == 'c')
+			{
+				char c = va_arg(fList, int);
+
+				write(1, &c, 1);
+				len++;
+			}
+			else if (*format == 's')
+			{
+				char *s = va_arg(fList, char *);
+
+				i = _strlen(s);
+				write(1, s, i);
+				len += i;
+			}
+			else if (*format == '%')
+			{
+				write(1, format, 1);
+				len++;
+			}
+			format++;
+			len++;
+		}
+	}
+	va_end(fList);
+	return (len);
+}
