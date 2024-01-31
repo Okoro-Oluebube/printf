eff5f07d610f7becc6efb482f26bf3d2b5c224f9
Edafe Akpevweoghene Merit <merytpeters@gmail.com>
2024-01-31 09:16:45 -0800

    Created a program for digit format specifiers

--- main.h
+++ main.h
@@ -7,5 +7,6 @@ int _printf(const char *format, ...);
 int _strlen(char *s);
 void printf_s(va_list fList, char *s);
 void printf_c(va_list flist, char c);
+void printf_nums(va_list fList, int num, unsigned int num);
 
 #endif
---
+++ printf_nums.c
@@ -0,0 +1,8 @@
+#include "main.h"
+
+/**
+ * printf_nums - Produces output according to format d, i, x, X
+ * u, b, o
+ * @num: Arguments
+ */
+void printf_nums(va_list fList, int num, unsigned int num)
